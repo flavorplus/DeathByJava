678f5ce351e38db3afbf311021848c524b4f35b2
Jonathan Vermeulen <jvermeulen@mbpvanjonathan.nbttech.com>
2018-08-01 14:19:49 +0100

    Adding the load generation capability

--- src/DeathByJava/src/main/java/com/riverbed/test/DeathByJava/service/DeathByJavaService.java
+++ src/DeathByJava/src/main/java/com/riverbed/test/DeathByJava/service/DeathByJavaService.java
@@ -4,11 +4,25 @@ package com.riverbed.test.DeathByJava.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class DeathByJavaService {
 
-	public String createLoad(Integer number) {
-		return "Hello Andre!! The number is:" + number;
-	}
+	private Random rng = new Random();
 
+	public String createLoad(Integer number) {
+		long startTime = System.currentTimeMillis();
+
+		for (int i = 0; i < (number*100); i++){
+			double r = rng.nextFloat();
+			double v = Math.sin(Math.cos(Math.sin(Math.cos(r))));
+		}
+
+		long runTime = System.currentTimeMillis() - startTime;
+
+		String output = String.format("Hello Jon!! The number of itterations was: %d The run time was:%dms", number, runTime);
+
+		return output;
+	}
 }
